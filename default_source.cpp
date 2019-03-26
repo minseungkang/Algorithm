30a211b7d58d6dad58a2b7730c163f595409c847
minseungkang <alstmd3@naver.com>
2019-03-26 21:40:39 +0900

    default_source.cpp 업로드

---
+++ default_source.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdio>
+#include <vector>
+
+#pragma warning (disable : 4996)
+
+#define INT_MAX 2147483647
+#define INT_MIN -2147483648
+#define LL long long
+
+using namespace std;
+
+int main() {
+
+	return 0;
+}
